9365177395d90c9613f2bd82b7b66a2fa53bd4ea
joyablue <dev.joyable@gmail.com>
2021-12-13 16:48:35 +0900

    211213 portfolio cotent

---
+++ flow/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+     // 슬라이드
+     var MainSwiper = new Swiper('.main-swiper .swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        loop: true,
+      });   
+});
